8bb14222028668060a39c12d5aebb7fa7c19098b
Maksim Levin <levmaxmih88@mail.ru>
2021-04-22 16:32:42 +0700

    Implementation of observeState

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/BaseViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/BaseViewModel.kt
@@ -1,8 +1,7 @@
 package ru.skillbranch.skillarticles.viewmodels
 
 import androidx.annotation.UiThread
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 
 abstract class BaseViewModel<T>(initState: T) : ViewModel() {
     protected val state: MediatorLiveData<T> = MediatorLiveData<T>().apply {
@@ -13,11 +12,30 @@ abstract class BaseViewModel<T>(initState: T) : ViewModel() {
     protected val currentstate
         get() = state.value!!
 
-    //лямбда выражение принимает в качестве аргумента лямбду в которую передается текущее состояние
-    // и она возвращает модифицированное состояние, которое присваивается текущему состоянию
+    /***
+     * лямбда выражение принимает в качестве аргумента лямбду в которую передается текущее состояние
+     * и она возвращает модифицированное состояние, которое присваивается текущему состоянию
+     */
     @UiThread
     protected inline fun updateState(update: (currentState: T) -> T) {
         val updateState: T = update(currentstate)
         state.value = updateState
+    }
+
+    /***
+     *более компактная форма записи observe принимает последним аргументом лямбда выражение обрабатывающее
+     *изменение текущего состояния
+     */
+    fun observeState(owner: LifecycleOwner, onChanged: (newState: T) -> Unit) {
+        state.observe(owner, Observer { onChanged(it!!) })
+    }
+}
+
+class ViewModelFactory(private val params: String) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ArtticleViewModel::class.java)) {
+            return ArticleViewModel(params) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
