3bc03b8a17b7d8167911d122e3613017d396e230
Maksim Levin <levmaxmih88@mail.ru>
2021-04-22 16:16:57 +0700

    Implementation of updateState

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/BaseViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/BaseViewModel.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.skillarticles.viewmodels
 
+import androidx.annotation.UiThread
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 
@@ -11,4 +12,12 @@ abstract class BaseViewModel<T>(initState: T) : ViewModel() {
     //not null current state
     protected val currentstate
         get() = state.value!!
+
+    //лямбда выражение принимает в качестве аргумента лямбду в которую передается текущее состояние
+    // и она возвращает модифицированное состояние, которое присваивается текущему состоянию
+    @UiThread
+    protected inline fun updateState(update: (currentState: T) -> T) {
+        val updateState: T = update(currentstate)
+        state.value = updateState
+    }
 }
