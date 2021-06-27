34ac073fb0eb3df408a37388cf4facc47df6462d
Maksim Levin <levmaxmih88@mail.ru>
2021-06-27 20:13:27 +0700

    Refactored UnorderedListSpan

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/UnorderedListSpan.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/UnorderedListSpan.kt
@@ -26,20 +26,29 @@ class UnorderedListSpan(
         lineTop: Int, lineBaseline: Int, lineBottom: Int, text: CharSequence?, lineStart: Int,
         lineEnd: Int, isFirstLine: Boolean, layout: Layout?
     ) {
+        //only for fist line draw bullet
         if (isFirstLine) {
-            val oldColor = paint.color
-            paint.color = bulletColor
-            canvas.drawCircle(
-                gapWidth + currentMarginLocation + bulletRadius,
-                (lineTop + lineBottom) / 2f,
-                bulletRadius,
-                paint
-            )
-            paint.color = oldColor
+            paint.withCustomColor {
+                canvas.drawCircle(
+                    gapWidth + currentMarginLocation + bulletRadius,
+                    (lineTop + lineBottom) / 2f,
+                    bulletRadius,
+                    paint
+                )
+            }
         }
     }
 
     private inline fun Paint.withCustomColor(block: () -> Unit) {
-        //TODO implement me
+        val oldColor = color
+        val oldStyle = style
+
+        color = bulletColor
+        style = Paint.Style.FILL
+
+        block()
+
+        color = oldColor
+        style = oldStyle
     }
 }
