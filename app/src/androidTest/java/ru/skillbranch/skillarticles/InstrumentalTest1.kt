8dd8ee19954119b868dc0b5d890520ac1dcaa9d5
Maksim Levin <levmaxmih88@mail.ru>
2021-06-27 17:39:17 +0700

    Created unordered list test

---
+++ app/src/androidTest/java/ru/skillbranch/skillarticles/InstrumentalTest1.kt
@@ -0,0 +1,112 @@
+package ru.skillbranch.skillarticles
+
+import android.graphics.*
+import android.graphics.drawable.VectorDrawable
+import android.text.Layout
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.view.View.TEXT_DIRECTION_FIRST_STRONG
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.mockk.*
+import junit.framework.Assert.assertEquals
+import org.junit.Assert
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+import org.junit.runner.RunWith
+import ru.skillbranch.skillarticles.markdown.spans.*
+
+
+/**
+ *  Instrumented test, which will execute on an Android device
+ *
+ *  See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+@RunWith(AndroidJUnit4::class)
+class InstrumentalTest1 {
+    companion object {
+        var scaleDensity: Float = 0f
+
+        @BeforeClass
+        @JvmStatic
+        fun setupClass() {
+            scaleDensity =
+                ApplicationProvider.getApplicationContext<App>().resources.displayMetrics.scaledDensity
+        }
+    }
+
+    //defaults
+    private val defaultColor = Color.GRAY
+    private val currentMargin = 0
+    private val lineTop = 0 //lineTop for line under test
+    private val lineBase = 14.dp() //lineBase for line under test
+    private val lineBottom = 20.dp() //lineBottom for line under test
+    private val defaultFontAscent = (-10).dp() //16sp fontsize
+    private val defaultFontDescent = (4).dp() //16sp fontsize
+    private val canvasWidth = 360.dp()
+
+    //mocks
+    private lateinit var canvas: Canvas
+    private lateinit var paint: Paint
+    private lateinit var layout: Layout
+
+    //text under test
+    private lateinit var text: SpannableString
+
+    //overwrite mocks before each test
+    @Before
+    fun setup() {
+        canvas = mockk(relaxed = true) { //overwrite only need functions other functions relaxed
+            every { getWidth() } returns canvasWidth
+        }
+        paint = mockk(relaxed = true) {
+            every { getColor() } returns defaultColor
+        }
+        layout = mockk(relaxed = true)
+
+        text = SpannableString("text")
+    }
+
+    @Test
+    fun unordered_list() {
+        //settings
+        val color = Color.RED
+        val gap = 8.dpf()
+        val radius = 4.dpf()
+
+        //set span on text
+        val span = UnorderedListSpan(gap, radius, color)
+        text.setSpan(span, 0, 0, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        //check leading margin
+        assertEquals((4 * radius + gap).toInt(), span.getLeadingMargin(true))
+
+
+        //bullet draw
+        span.drawLeadingMargin(
+            canvas, paint, currentMargin, TEXT_DIRECTION_FIRST_STRONG,
+            lineTop, lineBase, lineBottom, text, 0, text.length,
+            true, layout
+        )
+
+        //check order call
+        verifyOrder {
+            //check first set color to paint
+            paint.color = color
+            //check draw circle bullet
+            canvas.drawCircle(
+                gap + currentMargin + radius,
+                (lineTop + lineBottom) / 2f,
+                radius, paint
+            )
+            //check paint color restore
+            paint.color = defaultColor
+        }
+    }
+
+    private fun Int.dp() = (this * scaleDensity).toInt()
+    private fun Int.dpf() = this * scaleDensity
+}
