2092ec17bddf6e8ca9e972c98ef1f4aeabff8b13
Maksim Levin <levmaxmih88@mail.ru>
2022-01-06 18:09:32 +0300

    Implemented CommentsDataSource (33:23)

--- app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticleRepository.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticleRepository.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.skillarticles.data.repositories
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import androidx.paging.PagingSource
@@ -14,7 +15,7 @@ interface IArticleRepository {
     fun getAppSettings(): LiveData<AppSettings>
     fun updateSettings(appSettings: AppSettings)
     fun updateArticlePersonalInfo(info: ArticlePersonalInfo)
-    fun makeCommentDataSource(articleId: String) : CommentsDataSource
+    fun makeCommentDataSource(articleId: String): CommentsDataSource
 }
 
 class ArticleRepository(
@@ -53,23 +54,39 @@ class ArticleRepository(
 class CommentsDataSource(
     val articleId: String,
     val network: NetworkDataHolder
-) : PagingSource<Int, CommentRes>(){
+) : PagingSource<Int, CommentRes>() {
     override fun getRefreshKey(state: PagingState<Int, CommentRes>): Int? {
-        //TODO implement later
-        return null
+        val anchorPosition = state.anchorPosition
+            ?: return null //visible viewHolder position or null in initial load
+        val anchorPage = state.closestPageToPosition(anchorPosition) ?: return null //loaded page
+        val size = state.config.pageSize
+
+        val nextKey = anchorPage.nextKey
+        val prevKey = anchorPage.prevKey
+        val pageKey = prevKey?.plus(size) ?: nextKey?.minus(size) //if prev ==null -> initial load
+
+        Log.w(
+            "GET_REFRESH_KEY",
+            "anchorPosition$pageKey, offset:$pageKey prev:$prevKey, next$nextKey"
+        )
+        return pageKey
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CommentRes> {
-        val pageKey = params.key //offset
+        val pageKey = params.key ?: 0 //offset
         val pageSize = params.loadSize  //limit
 
         return try {
             val comments = network.loadComments(articleId, pageKey, pageSize)
             val prevKey = pageKey?.minus(pageSize)
-            val nextKey = if(comments.isNotEmpty()) pageKey?.plus(pageSize) else null
-            LoadResult.Page(data = comments, prevKey = prevKey, nextKey = nextKey)
+            val nextKey = if (comments.isNotEmpty()) pageKey?.plus(pageSize) else null
 
-        }catch (t:Throwable) {
+            Log.e(
+                "LOAD",
+                "load from network comments:${comments.size} offset:$pageKey limit:$pageSize prev:$prevKey next:$nextKey"
+            )
+            LoadResult.Page(data = comments, prevKey = prevKey, nextKey = nextKey)
+        } catch (t: Throwable) {
             LoadResult.Error(t)
         }
     }
